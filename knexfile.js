d662148d2b52f9e433df0f9648187910e3b656af
byeong0430 <byeong.kim0430@gmail.com>
2018-07-14 18:16:16 +0000

    changed production connection to see if app is connected to db

--- knexfile.js
+++ knexfile.js
@@ -24,15 +24,14 @@ module.exports = {
   production: {
     client: 'pg',
     debug: true,
-    connection: process.env.DATABASE_URL,
+    connection: process.env.DATABASE_URL + '?ssl=true',
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
       tableName: 'migrations'
-    },
-    ssl: true
+    }
   }
 
 };
