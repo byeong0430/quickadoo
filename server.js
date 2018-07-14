6f7914afe16c8552f20544678e6d39c39e4b10a1
byeong0430 <byeong.kim0430@gmail.com>
2018-07-14 06:51:42 +0000

    set up route for heroku psql

--- server.js
+++ server.js
@@ -12,6 +12,11 @@ const ENV = process.env.ENV || "development";
 const express = require("express");
 const bodyParser = require("body-parser");
 const sass = require("node-sass-middleware");
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+})
 
 // enable express app
 const app = express();
@@ -54,6 +59,17 @@ app.use(express.static(__dirname + '/public'));
 // ROUTES
 // Mount routes
 app.use('/', usersRoutes(knex));
+app.get('/db', async (req, res) => {
+  try {
+    const client = await pool.connect()
+    const result = await client.query('SELECT * FROM test_table');
+    res.render('pages/db', result);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+})
 
 // listen to port
 // if we set the IP address to '0.0.0.0', anyone can view the web page through my IP address
